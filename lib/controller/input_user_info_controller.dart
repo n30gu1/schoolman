036e51772a7daf7506d74c6217049fd28915f640
Seongheon Park <n30gu1@n30gu1.com>
2022-04-06 15:00:15 +0900

    Add Class Picker Map Builder

--- lib/controller/input_user_info_controller.dart
+++ lib/controller/input_user_info_controller.dart
@@ -6,7 +6,6 @@ class InputUserInfoController extends GetxController {
   late String schoolCode;
   late String regionCode;
   RxBool isLoading = true.obs;
-  RxList classInfo = [].obs;
 
   InputUserInfoController(this.regionCode, this.schoolCode);
 
@@ -19,11 +18,31 @@ class InputUserInfoController extends GetxController {
   void fetchClassInfo() async {
     isLoading.value = true;
     try {
-      classInfo.addAll(
+      createMap(
           await APIService.instance.fetchClassInfo(regionCode, schoolCode));
       isLoading.value = false;
     } catch (e) {
       log(e.toString());
     }
+  }
+
+  Future<Map<String, dynamic>> createMap(List classInfo) async {
+    Set grade = {};
+    Map classMap = {};
+
+    for (var element in classInfo) {
+      grade.add(element["GRADE"]);
+    }
+
+    for (var gradeElement in grade) {
+      classMap.addAll({gradeElement.toString(): Set()});
+      classInfo.forEach((element) {
+        if (element["GRADE"].toString().contains(gradeElement)) {
+          classMap[gradeElement.toString()].add(element["CLASS_NM"]);
+        }
+      });
+    }
+
+    return {"grades": grade, "classes": classMap};
   }
 }
