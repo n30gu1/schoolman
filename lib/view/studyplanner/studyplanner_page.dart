a8ef93824c70d84749526839649eb8207a42db56
n30gu1 <n@n30gu1.com>
2022-08-17 16:54:32 +0900

    Add Study Planner View

---
+++ lib/view/studyplanner/studyplanner_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class StudyPlannerPage extends StatelessWidget {
+  const StudyPlannerPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
